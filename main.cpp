0af145f1442f087179093cb363781d7125fd8323
lmnarka <lnarka09@gmail.com>
2022-03-05 09:43:04 +0500

    Sequential calculation A

--- main.cpp
+++ main.cpp
@@ -10,23 +10,23 @@
 using std::cout;
 
 float solution(float A, float den, int N) {
-	
-	for (int i = 0; i < N; i++) {
-		A *= 2 / sqrt(2)
-	}
+	A = 2 / den;
+	den = sqrt(2 + den);
+	return A;
 }
 
 int main()
 {
 	int N, rank, k, i;
-	
-
+	float den = sqrt(2);
+	float A = 2;
 	scanf("%d", &N);
-	/*if (N != 1) {
-		den = sqrt(2);
-		for (int i = 0; i < N; i++) {
-			A *= 2 / 
+	if (N != 1) {
+		for (int i = 0; i < N-1; i++) {
+			A *= 2 / den;
+			den = sqrt(2 + den);
 		}
-	}*/
+	}
+	cout << A;
 	
 }
