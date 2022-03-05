850ba61852c18086103bc8c428c2b86e4d3c1cd0
lmnarka <lnarka09@gmail.com>
2022-03-05 09:34:19 +0500

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <omp.h>
+#include <locale.h>
+#include <iostream>
+#include <windows.h>
+
+
+using std::cout;
+
+float solution(float A, float den, int N) {
+	
+	for (int i = 0; i < N; i++) {
+		A *= 2 / sqrt(2)
+	}
+}
+
+int main()
+{
+	int N, rank, k, i;
+	
+
+	scanf("%d", &N);
+	/*if (N != 1) {
+		den = sqrt(2);
+		for (int i = 0; i < N; i++) {
+			A *= 2 / 
+		}
+	}*/
+	
+}
